ab97a44a38ba8e1eb9592876a4bfbe27e6b8ab34
Joan <joan1998bocairent@gmail.com>
2025-02-12 10:59:01 +0100

    Examen correjido jajaja 12.0.0

---
+++ script.js
@@ -0,0 +1,25 @@
+bucle();
+
+let img = document.querySelector('#principal img');
+
+img.addEventListener('mouseover', (e) => {
+    e.target.style.width = "400px";
+});
+
+img.addEventListener('mouseout', (e) => {
+    e.target.style.width = "300px";
+});
+
+let btn = document.querySelector('.btn');
+
+btn.addEventListener('click',(e) => {
+    let nombre = document.querySelector('.text').value || '';
+
+    alert('Hola Usuario ( '+nombre+' ).')
+})
+
+document.body.addEventListener('dblclick',(e)=> {
+    let li = document.createElement('li');
+    li.textContent = 'Me gusta aprender cosas nuevas;'
+    document.querySelector('ul').appendChild(li);
+});
